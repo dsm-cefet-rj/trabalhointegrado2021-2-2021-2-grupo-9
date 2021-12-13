914b38b246fc7ff2cc18d10234d3b394cc6d718e
Flavio <flavioemx@gmail.com>
2021-12-13 19:56:27 -0300

    remove extra app.js

--- trovaoebaleias-new/src/views/App/App.js
+++ trovaoebaleias-new/src/views/App/App.js
@@ -4,6 +4,9 @@ import { Route, Switch } from 'react-router-dom'
 import { Home } from '../../views'
 import { Layout } from '../../components'
 
+import Contato from '../../pages/Contato'
+// import Login from '../../pages/Loginn'
+
 import './App.module.scss'
 
 const App = () => {
@@ -11,6 +14,7 @@ const App = () => {
      <Layout>
       <Switch>
         <Route exact path="/" component={Home} /> 
+        <Route path = '/contato' component={Contato} />
       </Switch>
     </Layout>
    )
