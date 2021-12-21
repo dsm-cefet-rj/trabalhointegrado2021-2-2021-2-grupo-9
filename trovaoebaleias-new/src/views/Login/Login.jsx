b7d05c2d491b9b11a1c6ef23c216c69c3b0a0c30
Flavio <flavioemx@gmail.com>
2021-12-21 11:54:06 -0300

    login: style login-card

--- trovaoebaleias-new/src/views/Login/Login.jsx
+++ trovaoebaleias-new/src/views/Login/Login.jsx
@@ -1,37 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./Login.module.scss"
 
 const Login = () => {
+    const [userName, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    const handleuserNameChange = (e) => setUserName(e.target.value)
+    const handlePasswordChange = (e) => setPassword(e.target.value)
+    const handleSubmit = () => {
+        console.log('data', userName, password)
+    }
+
     return ( 
-        <div className={styles.login} >
-            <section>
-            <form className="base">
+        <section className={styles.login}>
+            <form className={styles.base} onSubmit={handleSubmit}>
                 <p className={styles.titulo}>Login</p>
                 <input
                     type="text"
                     placeholder="Username"
                     id="login"
                     className={styles["input-padrao"]}
+                    value={userName}
+                    onChange={handleuserNameChange}
                 />
                 <input
                     type="password"
                     placeholder="Senha"
                     id="login"
                     className={styles["input-padrao"]}
+                    value={password}
+                    onChange={handlePasswordChange}
                 />
+                <input type="submit" className={styles["botao-ok"]} value="Entrar" />
             </form>
-
-            
-            </section>
-
-            <button className={styles["botao-ok"]}>
-            <Link to="/homeafter">Entrar</Link>
-            </button>
-            
-        </div>
+        </section>      
      );
 }
+
+            {/* <button className={styles["botao-ok"]}>
+                <Link to="/homeafter">Entrar</Link>
+            </button> */}
 
 export default Login;
