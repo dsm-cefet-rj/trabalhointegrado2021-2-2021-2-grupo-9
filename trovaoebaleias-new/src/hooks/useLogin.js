f36718869a8038523b9082a3dd0b0b9170a7a270
Flávio Almeida <flavioemx@gmail.com>
2021-12-21 15:32:53 -0300

    fluxo inicial de login (#5)
    * login: criar hook vazio de login
    
    * login: style login-card
    
    * login: check login and redirect
    
    * login: criar contexto de login
    
    * login: remove consoles na home

---
+++ trovaoebaleias-new/src/hooks/useLogin.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const useLogin = () => {
+    const [isLogged, setIsLogged] = useState(false)
+
+    return {
+        isLogged, 
+        setIsLogged,
+    }
+}
+ 
+export default useLogin;
--- trovaoebaleias-new/src/views/Login/Login.jsx
+++ trovaoebaleias-new/src/views/Login/Login.jsx
@@ -1,36 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Redirect } from 'react-router-dom'
+
+import { LoginContext } from "context";
 
 import styles from "./Login.module.scss"
 
+const LOGIN_TESTE = {
+    userName: "admin",
+    password: "123456"
+}
+
 const Login = () => {
+    const { setIsLogged } = useContext(LoginContext) || []
+    const [redirect, setRedirect] = useState(false)
+    const [userName, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleuserNameChange = (e) => setUserName(e.target.value)
+    const handlePasswordChange = (e) => setPassword(e.target.value)
+
+    const handleSubmit = () => {
+        const successfullLogin = 
+            userName === LOGIN_TESTE.userName && 
+            password === LOGIN_TESTE.password
+
+        if (successfullLogin) {
+            setIsLogged(true)
+            setRedirect(true)
+        }
+    }
+
+    if (redirect) {
+        return <Redirect push to="/home" />
+    }
+
     return ( 
-        <div className={styles.login} >
-            <section>
-            <form className="base">
+        <section className={styles.login}>
+            <form className={styles.base} onSubmit={handleSubmit}>
                 <p className={styles.titulo}>Login</p>
                 <input
                     type="text"
                     placeholder="Username"
                     id="login"
                     className={styles["input-padrao"]}
+                    value={userName}
+                    onChange={handleuserNameChange}
                 />
                 <input
                     type="password"
                     placeholder="Senha"
                     id="login"
                     className={styles["input-padrao"]}
+                    value={password}
+                    onChange={handlePasswordChange}
                 />
+                <input type="submit" className={styles["botao-ok"]} value="Entrar" />
             </form>
-
-            
-            </section>
-
-            <button className={styles["botao-ok"]}>
-            <Link to="/homeafter">Entrar</Link>
-            </button>
-            
-        </div>
+        </section>      
      );
 }
 
