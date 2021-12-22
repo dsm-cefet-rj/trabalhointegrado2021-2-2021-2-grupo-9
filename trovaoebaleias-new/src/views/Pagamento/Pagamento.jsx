7cb3a42753ec04a95793a67731d0b3f1a11e40ce
Flavio <flavioemx@gmail.com>
2021-12-22 11:22:25 -0300

    update-topbar: adiciona card à página de pagamento

--- trovaoebaleias-new/src/views/Pagamento/Pagamento.jsx
+++ trovaoebaleias-new/src/views/Pagamento/Pagamento.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import { useMediaQuery } from 'react-responsive';
+import React from "react";
 import { Link } from "react-router-dom";
+
+import { Card } from 'components'
 
 import Debito from "../../img/carddebit.svg";
 import Credito from "../../img/cardcredit.svg";
@@ -8,100 +9,97 @@ import Boleto from "../../img/boleto.svg";
 
 import Estilo from "./Pagamento.css"
 
-
-
 const Pagamento = () => {
     return(
-        <>
-        <div>
-        <h1 className="titulo">Formas de pagamento</h1>
-        
-        <fieldset className="formapag">           
-        <label class="radio"><input type="radio"/>Cartão de Débito</label>
-                <ul>
-                    <li><img src={Debito} className="imgsvg"/></li>
-                    <li><img src={Debito} className="imgsvg"/></li>
-                    <li><img src={Debito} className="imgsvg"/></li>
-                    <li><img src={Debito} className="imgsvg"/></li>
-                    <li><img src={Debito} className="imgsvg"/></li>
-                </ul>
-        </fieldset>
+        <Card>
+            <div>
+                <h1 className="titulo">Formas de pagamento</h1>
+                
+                <fieldset className="formapag">           
+                <label class="radio"><input type="radio"/>Cartão de Débito</label>
+                        <ul>
+                            <li><img src={Debito} className="imgsvg"/></li>
+                            <li><img src={Debito} className="imgsvg"/></li>
+                            <li><img src={Debito} className="imgsvg"/></li>
+                            <li><img src={Debito} className="imgsvg"/></li>
+                            <li><img src={Debito} className="imgsvg"/></li>
+                        </ul>
+                </fieldset>
 
-        <fieldset className="formapag">           
-        <label class="radio"><input type="radio"/>Cartão de Crédito</label>
-                <ul>
-                    <li><img src={Credito} className="imgsvg"/></li>
-                    <li><img src={Credito} className="imgsvg"/></li>
-                    <li><img src={Credito} className="imgsvg"/></li>
-                    <li><img src={Credito} className="imgsvg"/></li>
-                    <li><img src={Credito} className="imgsvg"/></li>
-                </ul>
-        </fieldset>
+                <fieldset className="formapag">           
+                <label class="radio"><input type="radio"/>Cartão de Crédito</label>
+                        <ul>
+                            <li><img src={Credito} className="imgsvg"/></li>
+                            <li><img src={Credito} className="imgsvg"/></li>
+                            <li><img src={Credito} className="imgsvg"/></li>
+                            <li><img src={Credito} className="imgsvg"/></li>
+                            <li><img src={Credito} className="imgsvg"/></li>
+                        </ul>
+                </fieldset>
 
-        <fieldset className="formapag">           
-        <label class="radio"><input type="radio"/>Boleto</label>
-                <ul>
-                    <li><img src={Boleto} className="imgsvg"/></li>
-                    <li><img src={Boleto} className="imgsvg"/></li>
-                </ul>
-        </fieldset>
-        <section>
-                <label for="numerocartao" class="text-pag">Número do Cartão</label>
-                <input type="number" id="numcartao" class="input-padrao" required/>
+                <fieldset className="formapag">           
+                <label class="radio"><input type="radio"/>Boleto</label>
+                        <ul>
+                            <li><img src={Boleto} className="imgsvg"/></li>
+                            <li><img src={Boleto} className="imgsvg"/></li>
+                        </ul>
+                </fieldset>
 
-                <label for="nomecartao" class="text-pag">Nome impresso no cartão</label>
-                <input type="text" id="nomecartao" class="input-padrao" required/>
+                <section>
+                        <label for="numerocartao" class="text-pag">Número do Cartão</label>
+                        <input type="number" id="numcartao" class="input-padrao" required/>
 
-                <label for="estado" class="text-pag">Validade</label>
-                <select class="amlista">
-                    <option>Mês</option>
-                    <option value="01">Janeiro</option>
-                    <option value="02">Fevereiro</option>
-                    <option value="03">Março</option>
-                    <option value="04">Abril</option>
-                    <option value="05">Maio</option>
-                    <option value="06">Junho</option>
-                    <option value="07">Julho</option>
-                    <option value="08">Agosto</option>
-                    <option value="09">Setembro</option>
-                    <option value="10">Outubro</option>
-                    <option value="11">Novembro</option>
-                    <option value="12">Dezembro</option>
-                </select>
+                        <label for="nomecartao" class="text-pag">Nome impresso no cartão</label>
+                        <input type="text" id="nomecartao" class="input-padrao" required/>
 
-                <select class="amlista">
-                    <option>Ano</option>
-                    <option value="21">2021</option>
-                    <option value="22">2022</option>
-                    <option value="23">2023</option>
-                    <option value="24">2024</option>
-                    <option value="25">2025</option>
-                    <option value="26">2026</option>
-                    <option value="27">2027</option>
-                    <option value="28">2028</option>
-                    <option value="29">2029</option>
-                    <option value="30">2030</option>
+                        <label for="estado" class="text-pag">Validade</label>
+                        <select class="amlista">
+                            <option>Mês</option>
+                            <option value="01">Janeiro</option>
+                            <option value="02">Fevereiro</option>
+                            <option value="03">Março</option>
+                            <option value="04">Abril</option>
+                            <option value="05">Maio</option>
+                            <option value="06">Junho</option>
+                            <option value="07">Julho</option>
+                            <option value="08">Agosto</option>
+                            <option value="09">Setembro</option>
+                            <option value="10">Outubro</option>
+                            <option value="11">Novembro</option>
+                            <option value="12">Dezembro</option>
+                        </select>
 
-                </select>
-            </section>
+                        <select class="amlista">
+                            <option>Ano</option>
+                            <option value="21">2021</option>
+                            <option value="22">2022</option>
+                            <option value="23">2023</option>
+                            <option value="24">2024</option>
+                            <option value="25">2025</option>
+                            <option value="26">2026</option>
+                            <option value="27">2027</option>
+                            <option value="28">2028</option>
+                            <option value="29">2029</option>
+                            <option value="30">2030</option>
 
-            <label for="cvv" class="text-pag">Código de Segurança</label>
-            <input type="number" id="numcartao" class="input-padrao" required/>
+                        </select>
+                    </section>
 
-            <label for="estado" class="text-pag">Parcelas</label>
-            <select name="estados-brasil" class="lista">
-                        <option value="1">1 mês - R$ 00,00</option>
-                        <option value="2">2 meses - R$ 00,00</option>
-                        <option value="3">3 meses - R$ 00,00</option>
-                        <option value="4">4 meses - R$ 00,00</option>
-                        <option value="5">5 meses - R$ 00,00</option>
-                        <option value="6">6 meses - R$ 00,00</option>
-                </select>
-        <Link to="/login"><input type="submit" value="Cadastrar" class="botao-ok"/></Link>
-            </div>
-            
-        </>
+                    <label for="cvv" class="text-pag">Código de Segurança</label>
+                    <input type="number" id="numcartao" class="input-padrao" required/>
 
+                    <label for="estado" class="text-pag">Parcelas</label>
+                    <select name="estados-brasil" class="lista">
+                                <option value="1">1 mês - R$ 00,00</option>
+                                <option value="2">2 meses - R$ 00,00</option>
+                                <option value="3">3 meses - R$ 00,00</option>
+                                <option value="4">4 meses - R$ 00,00</option>
+                                <option value="5">5 meses - R$ 00,00</option>
+                                <option value="6">6 meses - R$ 00,00</option>
+                        </select>
+                    <Link to="/login"><input type="submit" value="Cadastrar" class="botao-ok"/></Link>
+                </div>
+        </Card>
     )
 }
 
