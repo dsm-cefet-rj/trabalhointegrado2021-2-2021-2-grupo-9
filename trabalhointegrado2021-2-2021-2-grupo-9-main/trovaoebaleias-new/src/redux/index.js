b5d7249a5665f2dfd2a848bf56ca5f12cb155e94
Julie Moura <94178633+juliemoura@users.noreply.github.com>
2022-01-15 12:56:29 -0300

    commit redux planos

--- trabalhointegrado2021-2-2021-2-grupo-9-main/trovaoebaleias-new/src/redux/index.js
+++ trabalhointegrado2021-2-2021-2-grupo-9-main/trovaoebaleias-new/src/redux/index.js
@@ -1,5 +1,7 @@
 export { default as store } from './store'
 export { addBookReducer, updateBookReducer, deleteBookReducer } from './booksReducer'
 export { default as booksReducer } from './booksSlice'
+export { addPlanoReducer, updatePlanoReducer, deletePlanoReducer } from './planosReducer'
+export { default as planosReducer } from './planosSlice'
 export { addBook, updateBook, removeBook } from './booksSlice'
 
