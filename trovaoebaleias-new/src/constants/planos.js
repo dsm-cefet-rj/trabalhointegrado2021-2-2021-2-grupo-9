c5ca768d496d7a33333bb47e6c69fb785c7b7483
Julie Moura <juliemouradev@gmail.com>
2022-01-18 13:11:02 -0300

    planos constant commit

---
+++ trovaoebaleias-new/src/constants/planos.js
@@ -0,0 +1,22 @@
+const INITIAL_PLANOS= [
+    { 
+        id: 01,
+        nome: "Start",
+        desc1: "1 mês gratis",
+        desc2: "2 livros por mês"
+    },
+    { 
+        id: 02,
+        nome: "Pro",
+        desc1: "1 mês gratis",
+        desc2: "6 livros por mês"
+    },
+    { 
+        id: 03,
+        nome: "VIP",
+        desc1: "1 mês gratis",
+        desc2: "Livros ilimitados"
+    },
+]
+
+export { INITIAL_PLANOS }
