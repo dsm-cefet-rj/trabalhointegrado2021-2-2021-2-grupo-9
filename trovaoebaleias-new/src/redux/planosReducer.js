9a9f022ef4b6b12fb315dfd3cfe60d120dbb8a39
Julie Moura <juliemouradev@gmail.com>
2022-01-18 14:16:04 -0300

    reducer planos

---
+++ trovaoebaleias-new/src/redux/planosReducer.js
@@ -0,0 +1,28 @@
+import NotFound from "../img/not_found.svg";
+
+const getCurrentGreatestIndex = (state) => {
+  const planoIds = state.map(plano => +plano.id)
+  const sortedPlanoIdsDec = planoIds.sort((a, b) => b - a)
+  const firstIndex = sortedPlanoIdsDec[0]
+  return firstIndex
+}
+
+const addPlanoReducer = (state, payload) => {
+  const newPlano = { id: getCurrentGreatestIndex(state) + 1, src: NotFound, ...payload}
+  const newPlanoList = [...state, newPlano ]
+  return newPlanoList
+}
+
+const updatePlanoReducer = (state, payload) => {
+  const updatedPlano = payload
+  const remainingPlanos = [...state].filter(plano => plano.id !== +payload.id)
+  const updatedPlanoList = [...remainingPlanos, updatedPlano ]
+  return updatedPlanoList
+}
+
+const deletePlanoReducer = (state, payload) => {
+  const splicedPlanos = [...state].filter(plano => plano.id !== payload.id)
+  return splicedPlanos
+}
+
+export { addPlanoReducer, updatePlanoReducer, deletePlanoReducer }
