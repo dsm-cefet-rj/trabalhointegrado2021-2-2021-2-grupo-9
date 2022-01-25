0ae86467fee4fd346f69c5fa2fa4a27403af3bbe
Julie Moura <juliemouradev@gmail.com>
2022-01-25 19:34:27 -0300

    backend config

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3004
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
